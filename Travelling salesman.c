0fd108cfa9e85403a55beddf93c9bd89bef8580e
Rachanaa-Ghaju <113177997+Rachanaa-Ghaju@users.noreply.github.com>
2022-09-16 16:52:20 +0545

    Update Travelling salesman.c
    commited to get necessary inputs

--- Travelling salesman.c
+++ Travelling salesman.c
@@ -1,1 +1,29 @@
+void takeInput()
+{
+	int i,j;
 
+	printf("Enter the number of villages: ");
+	scanf("%d",&n);
+
+	printf("\nEnter the Cost Matrix\n");
+
+	for(i=0;i < n;i++)
+	{
+		printf("\nEnter Elements of Row: %d\n",i+1);
+
+		for( j=0;j < n;j++)
+			scanf("%d",&ary[i][j]);
+
+		completed[i]=0;
+	}
+
+	printf("\n\nThe cost list is:");
+
+	for( i=0;i < n;i++)
+	{
+		printf("\n");
+
+		for(j=0;j < n;j++)
+			printf("\t%d",ary[i][j]);
+	}
+}
