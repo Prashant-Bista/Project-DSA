9fb979a205c5b916645f0d7a068db699913cda55
Prashant Bista <bistaprashant1@gmail.com>
2022-09-23 22:23:14 +0545

    Update Travelling salesman.c
    least  function added

--- Travelling salesman.c
+++ Travelling salesman.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int array[10][10],completed[10],n,cost=0;
+int ary[10][10],completed[10],n,cost=0;
 void takeInput()
 {
 	int i,j;
@@ -43,6 +43,25 @@ if(ncity==999)
 ncity=0;
 printf("%d",ncity+1);
 cost+=ary[city][ncity];
- 
+}
 return;
+}
 
+int least(int c)
+{
+	int i,nc=999;
+	int min=999,kmin;
+	for (i=0;i<n;i++)
+	{
+		if((ary[c][i]!=0)&&(completed[i]==0)))
+		if(ary[c][i]+ary[i][c]<min)
+		{
+			min=ary[i][0]+ary[c][i];
+			kmin=ary[c][i];
+			nc=i;
+		}
+	}
+	if(min!=999)
+	cost+=kmin;
+	return nc;
+}
