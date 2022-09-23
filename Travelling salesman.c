90dfbf3c200540af058b39f9fda62b56e680922c
Prashant Bista <bistaprashant1@gmail.com>
2022-09-23 22:25:45 +0545

    Update Travelling salesman.c
    some changes

--- Travelling salesman.c
+++ Travelling salesman.c
@@ -14,7 +14,7 @@ void takeInput()
 		printf("\nEnter The cost to travel from city %d to others\n",i+1);
 
 		for( j=0;j < n;j++)
-			scanf("%d",&array[i][j]);
+			scanf("%d",&ary[i][j]);
 
 		completed[i]=0;
 	}
@@ -26,8 +26,26 @@ void takeInput()
 		printf("\n");
 
 		for(j=0;j < n;j++)
-			printf("\t%d",array[i][j]);
+			printf("\t%d",ary[i][j]);
 	}
+}
+int least(int c)
+{
+	int i,nc=999;
+	int min=999,kmin;
+	for (i=0;i<n;i++)
+	{
+		if((ary[c][i]!=0)&&(completed[i]==0))
+		if(ary[c][i]+ary[i][c]<min)
+		{
+			min=ary[i][0]+ary[c][i];
+			kmin=ary[c][i];
+			nc=i;
+		}
+	}
+	if(min!=999)
+	cost+=kmin;
+	return nc;
 }
 void mincost(int city)
 {
@@ -47,21 +65,4 @@ cost+=ary[city][ncity];
 return;
 }
 
-int least(int c)
-{
-	int i,nc=999;
-	int min=999,kmin;
-	for (i=0;i<n;i++)
-	{
-		if((ary[c][i]!=0)&&(completed[i]==0)))
-		if(ary[c][i]+ary[i][c]<min)
-		{
-			min=ary[i][0]+ary[c][i];
-			kmin=ary[c][i];
-			nc=i;
-		}
-	}
-	if(min!=999)
-	cost+=kmin;
-	return nc;
-}
+
